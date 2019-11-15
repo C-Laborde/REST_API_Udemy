472e483ebd9b274b6388e4151cec7c1daf979991
C-Laborde <cecilialaborde@gmail.com>
2019-11-15 14:55:52 +0100

    Fixed bug

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def get_stores():
 
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name, price):
+def create_item_in_store(name):
     request_data = request.get_json()
     for store in stores:
         if store["name"] == name:
